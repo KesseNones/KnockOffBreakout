5fb0477379d2c3088f29b4d6cb00c8ae32097974
KesseNones <jesse.jones@wsu.edu>
2023-09-08 14:29:03 -0700

    Added level victory message assets and loaded it

--- core/src/wsuv/bounce/BounceGame.java
+++ core/src/wsuv/bounce/BounceGame.java
@@ -22,6 +22,7 @@ public class BounceGame extends Game {
     public static final String RSC_EXPLOSION_FRAMES = "explosion8x8.png";
     public static final String RSC_GAMEOVER_IMG = "gameover.png";
     public static final String RSC_DEATH_IMG = "deathMessage.png";
+    public static final String RSC_LEVEL_WIN_IMG = "levelVictoryMessage.png";
     public static final String RSC_PRESSAKEY_IMG = "pressakey.png";
     public static final String RSC_BALL_IMG = "ball.png";
     public static final String RSC_PADDLE_IMG = "paddle.png";
@@ -71,6 +72,7 @@ public class BounceGame extends Game {
         am.load(RSC_SIX_HEALTH_BRICK_IMG, Texture.class);
         am.load(RSC_GAMEOVER_IMG, Texture.class);
         am.load(RSC_DEATH_IMG, Texture.class);
+        am.load(RSC_LEVEL_WIN_IMG, Texture.class);
         am.load(RSC_PRESSAKEY_IMG, Texture.class);
         am.load(RSC_EXPLOSION_FRAMES, Texture.class);
 
